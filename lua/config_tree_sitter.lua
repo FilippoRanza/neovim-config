0376683b184d826177f6a02089631b14ee72c1f5
Filippo Ranza <filipporanza@gmail.com>
2022-09-28 23:47:17 +0200

    Add forgotten files

---
+++ lua/config_tree_sitter.lua
@@ -0,0 +1,20 @@
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "java", "lua", "rust", "python", "latex", "scala" },
+
+  sync_install = false,
+
+  highlight = {
+    enable = true,
+  },
+  rainbow = {
+      enable = true,
+      extended_mode = true,
+  }
+}
+
+
+
+
+
