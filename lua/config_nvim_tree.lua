9ff37955d7cfd2db242b902cd7d56767b5e7643e
Filippo Ranza <filipporanza@gmail.com>
2023-02-02 17:33:45 +0100

    Update nvim-tree config

--- lua/config_nvim_tree.lua
+++ lua/config_nvim_tree.lua
@@ -2,8 +2,14 @@
 --
 
 
+
 require('nvim-tree').setup{
       prefer_startup_root = true,
-      open_on_setup = true,
       sync_root_with_cwd = true,
 }
+
+local function open_nvim_tree() 
+    require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
