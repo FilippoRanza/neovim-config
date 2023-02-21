217e80d8a083d45918f2cf41f95615cfc3f654f0
Filippo Ranza <filipporanza@gmail.com>
2023-02-21 23:27:28 +0100

    Use 4 spaces to indent C/C++

--- lua/formatter_conf.lua
+++ lua/formatter_conf.lua
@@ -1,9 +1,24 @@
+
+local util = require("formatter.util")
+local function clang_format()
+    return {
+        exe = "clang-format",
+        args = {
+          "-assume-filename",
+          util.escape_path(util.get_current_buffer_file_name()),
+          "--style={IndentWidth: 4}"
+        },
+        stdin = true,
+        try_node_modules = true,
+    }
+end
+
 require("formatter").setup {
     filetype = {
         python = require("formatter.filetypes.python").black,
         rust = require("formatter.filetypes.rust").rustfmt,
-        c = require("formatter.filetypes.c").clangformat,
-        cpp = require("formatter.filetypes.cpp").clangformat,
+        c = clang_format,
+        cpp = clang_format,
     },
 
     ["*"] = {
