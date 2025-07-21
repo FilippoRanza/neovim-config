24b5ed6025344619bde90435b0d88a3204b30228
FilippoRanza <filipporanza@gmail.com>
2025-07-21 11:09:02 +0200

    Jump to File support .c and .h files

--- lua/jump_to_file.lua
+++ lua/jump_to_file.lua
@@ -2,18 +2,22 @@ vim.api.nvim_set_keymap("n", "<space>gi", [[:lua jump_to_corresponding_file("edi
 vim.api.nvim_set_keymap("n", "<space>gv", [[:lua jump_to_corresponding_file("vsplit")<CR>]], { noremap = true, silent = true })
 
 function jump_to_corresponding_file(action)
-  local current_file = vim.fn.expand("%")
-  local new_file
+    local current_file = vim.fn.expand("%")
+    local new_file
 
-  if current_file:match("%.hpp$") then
-    new_file = current_file:gsub("%.hpp$", ".cpp")
-  elseif current_file:match("%.cpp$") then
-    new_file = current_file:gsub("%.cpp$", ".hpp")
-  else
-    print("Not an .hpp or .cpp file")
-    return
-  end
+    if current_file:match("%.hpp$") then
+        new_file = current_file:gsub("%.hpp$", ".cpp")
+    elseif current_file:match("%.cpp$") then
+        new_file = current_file:gsub("%.cpp$", ".hpp")
+    elseif current_file:match("%.h$") then
+        new_file = current_file:gsub("%.h$", ".c")
+    elseif current_file:match("%.c$") then
+        new_file = current_file:gsub("%.c$", ".h")
+    else
+        print("Not an .hpp, .cpp, .c, .h file")
+        return
+    end
 
-  vim.cmd(action .. " " .. new_file)
+    vim.cmd(action .. " " .. new_file)
 end
 
